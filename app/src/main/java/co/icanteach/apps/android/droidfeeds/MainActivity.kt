d03a3cb9bbdb7f375726ad14ff38fac51573b53f
muratcan.bur <6nz5--M2>
2020-12-08 16:00:02 +0300

    first commit

---
+++ app/src/main/java/co/icanteach/apps/android/droidfeeds/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.icanteach.apps.android.droidfeeds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
