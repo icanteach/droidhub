6e645283fb874bd8e4ea88fcaabf75179f00922c
muratcan.bur <muratcanbur@gmail.com>
2020-12-14 23:48:57 +0300

    nav component & bottom nav view.

--- app/src/main/java/co/icanteach/apps/android/droidfeeds/MainActivity.kt
+++ app/src/main/java/co/icanteach/apps/android/droidfeeds/MainActivity.kt
@@ -1,13 +1,25 @@
 package co.icanteach.apps.android.droidfeeds
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import co.icanteach.apps.android.droidfeeds.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navHostFragment.navController)
     }
+
 }
