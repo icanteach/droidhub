a11fee3c4f8464349d7573111a89a1b657eeca06
muratcanbur <muratcanbur@gmail.com>
2022-10-11 08:11:00 +0300

    WIP

---
+++ app/src/main/java/co/icanteach/apps/android/droidhub/features/feed/FeedScreenUiState.kt
@@ -0,0 +1,7 @@
+package co.icanteach.apps.android.droidhub.features.feed
+
+import co.icanteach.apps.android.droidhub.features.feedcomponents.ComponentItem
+
+data class FeedScreenUiState(
+    val components: List<ComponentItem> = emptyList()
+) 
---
+++ app/src/main/java/co/icanteach/apps/android/droidhub/features/feedcomponents/ComponentItem.kt
@@ -0,0 +1,5 @@
+package co.icanteach.apps.android.droidhub.features.feedcomponents
+
+interface ComponentItem {
+    val id: String
+}
